80194db61235709a2c6d06956a37af14331b5f1b
TruongNM <manhtruonghedspi@gmail.com>
2018-05-25 17:09:02 +0700

    Add webcam demo, update readme

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# multi-task-learning
+# Smile detection, gender and age estimation using Multi-task Learning
+
+On the original paper [DEX: Deep EXpectation](https://www.vision.ee.ethz.ch/en/publications/papers/proceedings/eth_biwi_01229.pdf) the authors were able to display remarkable results in classifying the age of an individual based on a given image alone.
+
+Let see how accuracy (bad I guess), with limited resources, we can get with self-construct architecture. And not only age, we also classifying gender and smile by using multi-task training technique.
+
+## Dependencies
+* Python 3.x
+* OpenCV 2
+* tensorflow
+* numpy
+* pandas
+* matplotlib
+* scipy
+* seaborn
+* [MTCNN](https://github.com/ipazc/mtcnn) for face detection
+
+## Usage
+### Download datasets
+First download [GENKI4K](https://github.com/ipazc/mtcnn) and [IMDB-WIKI](https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/) (cropped face version) dataset.
+
+### Preprocess data
+First run [imdb-preprocess.ipynb](./imdb-preprocess.ipynb). This step preprocess IMDB-WIKI dataset, filter out images with more than one face, gender is NaN and invalid age. All saved in a pickle file.
+
+Next run [prepare-data.ipynb](./prepare-data.ipynb). This will split two datasets in to training parts and test parts. The IMDB-WIKI dataset we will split into two separate datasets, gender and age datasets. So we have 3 datasets: age gender age. Each file will run into MTCNN network to detect and crop face, resize to 48x48 and convert to gray, then all datasets are saved into pickle files.
+
+### Training
+Run [CNN2Head_train.ipynb](./CNN2Head_train.ipynb), using exponential decay to reduce learning rate over time. Change your datasets folder links, training parameters in [const.py](./const.py). Tensorboard's log will be saved in `summary` folder, training result in each batch is written in [log.csv](./log.csv).
+```python
+WEIGHT_INIT = 0.01
+NUM_TASKS = 3
+IMG_SIZE = 48
+INIT_LR = 0.01
+USE_BN = True
+BN_DECAY = 0.99
+EPSILON = 0.001
+WEIGHT_DECAY = 0.01
+DECAY_STEP = 1000
+DECAY_LR_RATE = 0.95
+BATCH_SIZE = 128
+
+USE_GPU = True
+SAVE_FOLDER = './save/current/'
+NUM_EPOCHS = 2000
+DROP_OUT_PROB = 0.5
+```
+
+### Evaluation
+Run [evaluate.ipynb](./evaluate.ipynb) to evaluate learned model on test datasets.
+
+### Testing
+For single file, see [test.ipynb](./test.ipynb) file for more example.
+
+![demo single file](./screenshots/predict.png)
+
+For demo on webcam, run [demo.py](./demo.py) file.
+
+![demo webcam](./screenshots/demo.gif)
+
+### TODO
+* Augmentation for balanced label, reduce overfitting
+* Improve accuracy
+
+### References and Acknowledgments
+This code is part of my GR project at HUST University. Following papers and codes are referred:
+1. Dinh Viet Sang, Le Tran Bao Cuong, and Do Phan Thuan. 2017., Facial Smile Detection Using Convolutional Neural Networks. In The 9th International Conference on Knowledge and Systems Engineering (KSE 2017). 138–143.
+2. Dinh Viet Sang, Le Tran Bao Cuong, Pham Thai Ha, Multi-task learning for smile detection, emotion recognition and gender classification, December 2017
+3. [IMDB-WIKI: trying a small model for age classification
+](https://surfertas.github.io/deeplearning/2017/04/18/imdbwiki.html)
+3. [MultiTask-MergeDataset - Le Tran Bao Cuong](https://gitlab.com/ltbclqd2805/MultiTask-MergeDataset)
---
+++ demo.py
@@ -0,0 +1,103 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import BKNetStyle
+from const import *
+from mtcnn.mtcnn import MTCNN
+
+
+# In[2]:
+
+
+def load_network():
+    sess = tf.Session()
+    x = tf.placeholder(tf.float32, [None, 48, 48, 1])
+    y_smile_conv, y_gender_conv, y_age_conv, phase_train, keep_prob = BKNetStyle.BKNetModel(x)
+    print('Restore model')
+    saver = tf.train.Saver()
+    saver.restore(sess, SAVE_FOLDER + 'model.ckpt')
+    print('OK')
+    return sess, x, y_smile_conv, y_gender_conv, y_age_conv, phase_train, keep_prob
+
+
+# In[3]:
+
+
+def draw_label(image, x, y, w, h, label, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, thickness=2):
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0,155,255), 2)
+    cv2.putText(image, label, (x,y), font, font_scale, (255, 255, 255), thickness)
+
+
+# In[4]:
+
+
+def main(sess, x, y_smile_conv, y_gender_conv, y_age_conv, phase_train, keep_prob):
+    # capture video
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    detector = MTCNN()
+
+    while True:
+        # get video frame
+        ret, img = cap.read()
+
+        if not ret:
+            print("error: failed to capture image")
+            return -1
+
+        # detect face and crop face, convert to gray, resize to 48x48
+        original_img = img
+        result = detector.detect_faces(original_img)
+        if not result:
+            cv2.imshow("result", original_img)
+            continue
+        face_position = result[0].get('box')
+        x_coordinate = face_position[0]
+        y_coordinate = face_position[1]
+        w_coordinate = face_position[2]
+        h_coordinate = face_position[3]
+        img = original_img[y_coordinate:y_coordinate+h_coordinate, x_coordinate:x_coordinate+w_coordinate]
+        if(img.size==0):
+            cv2.imshow("result", original_img)
+            continue;
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img,(48, 48))
+        img = (img - 128) / 255.0
+        T = np.zeros([48, 48, 1])
+        T[:, :, 0] = img
+        test_img = []
+        test_img.append(T)
+        test_img = np.asarray(test_img)
+
+        predict_y_smile_conv = sess.run(y_smile_conv, feed_dict={x: test_img, phase_train: False, keep_prob: 1})
+        predict_y_gender_conv = sess.run(y_gender_conv, feed_dict={x: test_img, phase_train: False, keep_prob: 1})
+        predict_y_age_conv = sess.run(y_age_conv, feed_dict={x: test_img, phase_train: False, keep_prob: 1})
+
+        smile_label = "-_-" if np.argmax(predict_y_smile_conv)==0 else ":)"
+        gender_label = "Female" if np.argmax(predict_y_gender_conv)==0 else "Male"
+        argmax_predict_age = np.argmax(predict_y_age_conv)
+
+        label = "{}, {}, {}".format(smile_label, gender_label, argmax_predict_age)
+        draw_label(original_img, x_coordinate, y_coordinate, w_coordinate, h_coordinate, label)
+
+        cv2.imshow("result", original_img)
+        key = cv2.waitKey(1)
+
+        if key == 27:
+            break
+
+
+# In[5]:
+
+
+if __name__ == '__main__':
+    sess, x, y_smile_conv, y_gender_conv, y_age_conv, phase_train, keep_prob = load_network()
+    main(sess, x, y_smile_conv, y_gender_conv, y_age_conv, phase_train, keep_prob)
